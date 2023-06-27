9b88da9593b23644a59dd87ef36cd290695bad7b
Steven Wang <steven@luabase.com>
2023-06-27 17:34:23 -0400

    remove dupe propert_funding_m_

--- models/tmp/stg_hubspot__company_tmp.sql
+++ models/tmp/stg_hubspot__company_tmp.sql
@@ -5,6 +5,5 @@ select
     case
         when property_total_money_raised like '%M' then split(REGEXP_REPLACE(property_total_money_raised, '[$,,]', ''), 'M')[0]::float
         when property_total_money_raised like '%K' then split(REGEXP_REPLACE(property_total_money_raised, '[$,,]', ''), 'K')[0]::float / 1000
-    end as property_total_money_raised_cleaned,
-    property_funding_m_
+    end as property_total_money_raised_cleaned
 from {{ var('company') }}

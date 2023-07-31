80fd5f4e3ea7937a33bdf6fe6cc04d22ceaa7d16
Steven Wang <steven@luabase.com>
2023-07-31 12:00:54 -0400

    Add pound symbol to regex

--- models/tmp/stg_hubspot__company_tmp.sql
+++ models/tmp/stg_hubspot__company_tmp.sql
@@ -3,7 +3,7 @@
 select 
     *,
     case
-        when property_total_money_raised like '%M' then split(REGEXP_REPLACE(property_total_money_raised, '[$,,]', ''), 'M')[0]::float
-        when property_total_money_raised like '%K' then split(REGEXP_REPLACE(property_total_money_raised, '[$,,]', ''), 'K')[0]::float / 1000
+        when property_total_money_raised like '%M' then split(REGEXP_REPLACE(property_total_money_raised, '[€,$,,]', ''), 'M')[0]::float
+        when property_total_money_raised like '%K' then split(REGEXP_REPLACE(property_total_money_raised, '[€,$,,]', ''), 'K')[0]::float / 1000
     end as property_total_money_raised_cleaned
 from {{ var('company') }}
